23b3758bee4857e67cb541689a8077de3de792da
Jared <w13431838023@gmail.com>
2024-09-19 13:22:47 +0800

    built for deploy

--- src/views/About.vue
+++ src/views/About.vue
@@ -75,19 +75,11 @@
             </div>
             <div class="button-container">
                 <button @click="backToMenu" class="btn-with-bg">
-<<<<<<< HEAD
                     <span class="tibetan-text">ཕྱིར། </span> <!-- 藏文文本 -->
                     <span class="chinese-text">回到主页</span>
                 </button>
                 <button @click="startGame" class="btn-with-bg">
                     <span class="tibetan-text">ཐོག་མར་རོལ་རྩེད། </span> <!-- 藏文文本 -->
-=======
-                    <span class="tibetan-text">གཞུང་ཚིག</span> <!-- 藏文文本 -->
-                    <span class="chinese-text">回到主页</span>
-                </button>
-                <button @click="startGame" class="btn-with-bg">
-                    <span class="tibetan-text">གཞུང་ཚིག</span> <!-- 藏文文本 -->
->>>>>>> 2deed6aa0f1a9a94136ed10c75ef164358e3ad1a
                     <span class="chinese-text">开始游戏</span>
                 </button>
             </div>
